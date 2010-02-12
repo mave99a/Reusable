ed07f3f2737f6fef9472a0f66b081dea45c4830e
Robert Mao <zhihong.mao@gmail.com>
2010-02-11 23:37:25 -0800

    improved paginatortag and makeobjectlist tag

--- objectlisttag/templatetags/makeobjectlist.py
+++ objectlisttag/templatetags/makeobjectlist.py
@@ -15,9 +15,15 @@ from rendertag.templatetags.parseargshelper import parse_args_kwargs_and_as_var
 register = template.Library()
 
 class MakeObjectListNode(template.Node):
-    def __init__(self, object_ref, as_var = 'object_list',):
+    def __init__(self, object_ref, as_var, paginate_by, addtion_filter):
         self.object_ref = template.Variable(object_ref)
         self.as_var = as_var
+        try: 
+            self.paginate_by = int(paginate_by)
+        except: 
+            self.paginate_by = 10
+            
+        self.addtion_filter = addtion_filter
             
     def render(self, context):
         try: 
@@ -28,11 +34,34 @@ class MakeObjectListNode(template.Node):
         if self.as_var is None:
             self.as_var = 'object_list'
         
-        queryset = eval("object.order('-mtime')")    
-        paginator = Paginator(queryset, 3,  allow_empty_first_page=True)
-        page_obj = paginator.page(1)
+        if self.addtion_filter is not None: 
+            queryset = eval("object." + self.addtion_filter)
+        else:
+            queryset = object
+            
+        paginator = Paginator(queryset, self.paginate_by,  allow_empty_first_page=True)
+        
+        request = context['request']
+        page = request.GET.get('page', 1)
+            
+        try:
+            page_number = int(page)
+        except ValueError:
+            if page == 'last':
+                page_number = paginator.num_pages
+            else:
+                # Page is not 'last', nor can it be converted to an int.
+                page_number = 1
+
+        try:
+            page_obj = paginator.page(page_number)
+        except InvalidPage:
+            page_obj = paginator.page(1)
             
         context[self.as_var] = page_obj.object_list
+        context['paginator'] = paginator
+        context['page_obj'] = page_obj
+
         return ''
         
     
@@ -43,6 +72,8 @@ def do_make_object_list(parser, token):
     else: 
         args, kwargs, as_var = parse_args_kwargs_and_as_var(parser, bits[1:])
         return MakeObjectListNode(args[0], 
+                                paginate_by = kwargs.get("paginate_by"),
+                                addtion_filter = kwargs.get("addtion_filter"),
                                 as_var = as_var)
 
 register.tag('makeobjectlist', do_make_object_list)
--- paginatortag/templatetags/paginator.py
+++ paginatortag/templatetags/paginator.py
@@ -2,6 +2,8 @@
 #  refer: http://blog.localkinegrinds.com/2007/09/06/digg-style-pagination-in-django/
 #  styles: http://mis-algoritmos.com/2007/03/16/some-styles-for-your-pagination/
 #
+#  has been modified to use the "page_obj" and "paginator" only from context
+#
 from django import template
  
 register = template.Library()
@@ -12,35 +14,39 @@ NUM_PAGES_OUTSIDE_RANGE = 2
 ADJACENT_PAGES = 4
  
 def paginator(context):
-    if (context["is_paginated"]):
+    page_obj = context["page_obj"]
+    paginator = context["paginator"]
+    if (page_obj.has_other_pages()):
         " Initialize variables "
+        page = page_obj.number
+        pages =  paginator.num_pages
         in_leading_range = in_trailing_range = False
         pages_outside_leading_range = pages_outside_trailing_range = range(0)
  
-        if (context["pages"] <= LEADING_PAGE_RANGE_DISPLAYED):
+        if (pages <= LEADING_PAGE_RANGE_DISPLAYED):
             in_leading_range = in_trailing_range = True
-            page_numbers = [n for n in range(1, context["pages"] + 1) if n > 0 and n <= context["pages"]]           
-        elif (context["page"] <= LEADING_PAGE_RANGE):
+            page_numbers = [n for n in range(1, pages + 1) if n > 0 and n <= pages]           
+        elif (page <= LEADING_PAGE_RANGE):
             in_leading_range = True
-            page_numbers = [n for n in range(1, LEADING_PAGE_RANGE_DISPLAYED + 1) if n > 0 and n <= context["pages"]]
-            pages_outside_leading_range = [n + context["pages"] for n in range(0, -NUM_PAGES_OUTSIDE_RANGE, -1)]
-        elif (context["page"] > context["pages"] - TRAILING_PAGE_RANGE):
+            page_numbers = [n for n in range(1, LEADING_PAGE_RANGE_DISPLAYED + 1) if n > 0 and n <= pages]
+            pages_outside_leading_range = [n + pages for n in range(0, -NUM_PAGES_OUTSIDE_RANGE, -1)]
+        elif (page > pages - TRAILING_PAGE_RANGE):
             in_trailing_range = True
-            page_numbers = [n for n in range(context["pages"] - TRAILING_PAGE_RANGE_DISPLAYED + 1, context["pages"] + 1) if n > 0 and n <= context["pages"]]
+            page_numbers = [n for n in range(pages - TRAILING_PAGE_RANGE_DISPLAYED + 1, pages + 1) if n > 0 and n <= pages]
             pages_outside_trailing_range = [n + 1 for n in range(0, NUM_PAGES_OUTSIDE_RANGE)]
         else: 
-            page_numbers = [n for n in range(context["page"] - ADJACENT_PAGES, context["page"] + ADJACENT_PAGES + 1) if n > 0 and n <= context["pages"]]
-            pages_outside_leading_range = [n + context["pages"] for n in range(0, -NUM_PAGES_OUTSIDE_RANGE, -1)]
+            page_numbers = [n for n in range(page - ADJACENT_PAGES, page + ADJACENT_PAGES + 1) if n > 0 and n <= pages]
+            pages_outside_leading_range = [n + pages for n in range(0, -NUM_PAGES_OUTSIDE_RANGE, -1)]
             pages_outside_trailing_range = [n + 1 for n in range(0, NUM_PAGES_OUTSIDE_RANGE)]
         return {
-            "is_paginated": context["is_paginated"],
-            "previous": context["previous"],
-            "has_previous": context["has_previous"],
-            "next": context["next"],
-            "has_next": context["has_next"],
-            "results_per_page": context["results_per_page"],
-            "page": context["page"],
-            "pages": context["pages"],
+            "is_paginated": page_obj.has_other_pages(),
+            "previous": page_obj.previous_page_number(),
+            "has_previous": page_obj.has_previous(),
+            "next":  page_obj.next_page_number(),
+            "has_next":  page_obj.has_next(),
+            "results_per_page": paginator.per_page,
+            "page": page,
+            "pages": pages,
             "page_numbers": page_numbers,
             "in_leading_range" : in_leading_range,
             "in_trailing_range" : in_trailing_range,
